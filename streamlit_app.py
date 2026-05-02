781137a0f1a94fb32d9bf8e9642a9dd69a057cdd
CuongMinz <a52020@st.thanglong.edu.vn>
2026-05-02 16:36:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,3 +18,38 @@ try:
 
 except:
     st.error("❌ Không tìm thấy file data.csv")
+
+# ======================
+# CLEAN DATA
+# ======================
+
+# Mapping
+mapping_year = {
+    "Năm 1": 1,
+    "Năm 2": 2,
+    "Năm 3": 3,
+    "Năm 4": 4
+}
+
+mapping_credit = {
+    "Dưới 14": 12,
+    "14–16": 15,
+    "17–19": 18,
+    "20–22": 21,
+    "Trên 22": 23
+}
+
+mapping_khoiluong = {
+    "Nhẹ": 1,
+    "Vừa phải": 2,
+    "Hơi nặng": 3,
+    "Rất nặng": 4
+}
+
+# Apply
+df["NamHoc"] = df["Bạn đang học năm mấy?"].map(mapping_year)
+df["TinChi"] = df["Một học kỳ bạn thường học bao nhiêu tín chỉ?"].map(mapping_credit)
+df["KhoiLuong"] = df["Bạn cảm thấy khối lượng học tập của mình:"].map(mapping_khoiluong)
+
+st.subheader("📊 Dữ liệu sau khi chuẩn hóa")
+st.dataframe(df)
